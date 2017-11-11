b153b01dd543d479936a72c6f0cfe5cb50326c0e
Greg Bergé <berge.greg@gmail.com>
2017-11-11 14:42:28 +0000

    [ex-2] create schema

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
-console.log('Hello World!')
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
+
+const typeDefs = /* GraphQL */ `
+  enum Gender {
+    MALE
+    FEMALE
+    UNKNOWN
+  }
+
+  type Character {
+    id: ID!
+    name: String!
+    height: Int
+    gender: Gender
+  }
+
+  type Query {
+    character(id: ID!): Character
+  }
+`
+
+const schema = makeExecutableSchema({ typeDefs })
