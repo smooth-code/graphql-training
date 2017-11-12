cc1f22a6228e8b470a99c3433bd8f5b5c2df65b3
Greg Bergé <berge.greg@gmail.com>
2017-11-12 17:01:43 +0000

    [ex-14] directives

--- src/schema.test.js
+++ src/schema.test.js
@@ -35,5 +35,56 @@ describe('GraphQL schema', () => {
         },
       })
     })
+
+    it('should work with directive', async () => {
+      const query = /* GraphQL */ `
+        query characterById($id: ID!, $withWeapons: Boolean = false) {
+          character(id: $id) {
+            name
+            weapons @include(if: $withWeapons) {
+              name
+            }
+          }
+        }
+      `
+      const swapi = {
+        async getPerson() {
+          return {
+            name: 'James',
+            created: new Date('2017-05-21'),
+            gender: 'male',
+          }
+        },
+      }
+
+      const resultWithoutWeapons = await graphql(
+        schema,
+        query,
+        null,
+        { swapi },
+        { id: 1 },
+      )
+
+      expect(resultWithoutWeapons.data).toEqual({
+        character: {
+          name: 'James',
+        },
+      })
+
+      const resultWithWeapons = await graphql(
+        schema,
+        query,
+        null,
+        { swapi },
+        { id: 1, withWeapons: true },
+      )
+
+      expect(resultWithWeapons.data).toEqual({
+        character: {
+          name: 'James',
+          weapons: [],
+        },
+      })
+    })
   })
 })
