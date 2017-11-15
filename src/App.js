6aaa706ece9eae4e2e57aca113ef7bda61c19acc
Greg Bergé <berge.greg@gmail.com>
2017-11-15 14:09:35 +0000

    [ex-17] mutations

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React from 'react'
 import gql from 'graphql-tag'
-import { graphql } from 'react-apollo'
+import { graphql, compose } from 'react-apollo'
 
-const QUERY = gql`
-  query character($id: ID!) {
-    character(id: $id) {
-      name
-      films {
-        id
-        title
-      }
-    }
-  }
-`
-
-const App = ({ data }) => (
+const App = ({ data, onSubmit }) => (
   <div className="app">
     <header>
       <h1>Star Wars React</h1>
@@ -26,10 +14,64 @@ const App = ({ data }) => (
           {data.character.films.map(film => (
             <div key={film.id}>{film.title}</div>
           ))}
+          <h2>Weapons</h2>
+          {data.character.weapons.map(weapon => (
+            <div key={weapon.name}>{weapon.name}</div>
+          ))}
+          <form onSubmit={onSubmit}>
+            <input name="weapon" placeholder="Weapon" />
+            <button>Add Weapon</button>
+          </form>
         </div>
       )}
     </div>
   </div>
 )
 
-export default graphql(QUERY, { options: { variables: { id: 1 } } })(App)
+const withData = graphql(
+  gql`
+    query character($id: ID!) {
+      character(id: $id) {
+        name
+        weapons {
+          name
+        }
+        films {
+          id
+          title
+        }
+      }
+    }
+  `,
+  {
+    options: { variables: { id: 1 } },
+  },
+)
+
+const withMutate = graphql(
+  gql`
+    mutation addCharacterWeapon($characterId: ID!, $weapon: WeaponInput!) {
+      addCharacterWeapon(characterId: $characterId, weapon: $weapon) {
+        name
+      }
+    }
+  `,
+  {
+    props: ({ mutate, ownProps: { data } }) => ({
+      async onSubmit(event) {
+        event.preventDefault()
+        await mutate({
+          variables: {
+            characterId: data.variables.id,
+            weapon: {
+              name: event.target.weapon.value,
+            },
+          },
+        })
+        data.refetch()
+      },
+    }),
+  },
+)
+
+export default compose(withData, withMutate)(App)
